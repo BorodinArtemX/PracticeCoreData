fe7564eab0c2c856cdc44a98c1d897405d32fedf
BorodinArtemX <BorodinArtemX@icloud.com>
2023-07-05 18:47:44 +0200

    Create Project

---
+++ Practice CoreData ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  Practice CoreData ToDoList
+//
+//  Created by Артем Бородин on 05/07/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ Practice CoreData ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice CoreData ToDoList
+//
+//  Created by Артем Бородин on 05/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
